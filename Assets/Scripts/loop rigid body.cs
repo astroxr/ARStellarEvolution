bae533e5709afdb0fc335b0e10c4372c3884c182
muazburhan <42548013+muazburhan@users.noreply.github.com>
2019-05-26 15:02:30 -0700

    Add files via upload

---
+++ Assets/Scripts/loop rigid body.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * test code to see if simple rigid body 
+ * would have the same black background problem
+ */
+
+public class Loop : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -10)
+            Respawn();
+    }
+
+    private void Respawn()
+    {
+        transform.position = new Vector3(0, 10);
+
+        var fall = GetComponent<Rigidbody>();
+        fall.velocity = Vector3.zero;
+    }
+}
