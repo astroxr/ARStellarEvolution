a1d60e3fa968ae7091f2d7a68e33b82c6e779c53
muazburhan <muazburhan@users.noreply.github.com>
2019-06-21 14:43:08 -0700

    Added some new text
    Just add some new simple text

--- ARStar/MassiveStar.swift
+++ ARStar/MassiveStar.swift
@@ -25,27 +25,27 @@ class MassiveStar: UIViewController {
 
     @IBAction func changeSlider2(_ sender: UISlider) {
         if(sender.value < 1){
-            phaseNumber2.text = "Phase 1"
+            phaseNumber2.text = "Phase 1: Nebula"
             descPhase2.text = "Huge clouds of dust and gases exist in space."
             starImage2.image = UIImage(named: "nebulacloud")
         }
         else if(sender.value < 2){
-            phaseNumber2.text = "Phase 2"
+            phaseNumber2.text = "Phase 2: Massive Star"
             descPhase2.text = "The dust and gases are attracted to each other by their own gravity and getting denser and hotter until they fused together form a star."
             starImage2.image = UIImage(named: "massivestar")
         }
         else if(sender.value < 3){
-            phaseNumber2.text = "Phase 3"
+            phaseNumber2.text = "Phase 3: Red Super Giant"
             descPhase2.text = "After the star has run out of hydrogen, it will lose its hydrostatic equilibrium and it is going to collapse again until the heliums themselves will be used as an energy source, which will then expand the star even more."
             starImage2.image = UIImage(named: "redsupergiant")
         }
         else if(sender.value < 4){
-            phaseNumber2.text = "Phase 4"
+            phaseNumber2.text = "Phase 4: Supernova"
             descPhase2.text = "The star expand so much that its outer atmosphere drifted out from the star in a process called supernova explosion."
             starImage2.image = UIImage(named: "nebulacloud")
         }
         else {
-            phaseNumber2.text = "Phase 5"
+            phaseNumber2.text = "Phase 5: Black Hole"
             descPhase2.text = "The remnants of the massive star is the black hole, which is the core of the star. It is incredibly dense and hot."
             starImage2.image = UIImage(named: "blackhole")
         }
