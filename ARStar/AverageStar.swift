952065222ef3152d2d9248ff7df32231faf7facd
muazburhan <muazburhan@users.noreply.github.com>
2019-06-21 14:24:36 -0700

    I added the native iOS app for iphone
    This is files for the iOS app made in xcode using swift language

---
+++ ARStar/AverageStar.swift
@@ -0,0 +1,53 @@
+//
+//  AverageStar.swift
+//  ARStar
+//
+//  Created by ual-laptop on 6/17/19.
+//  Copyright © 2019 AstroCardboard. All rights reserved.
+//
+
+import UIKit
+
+class AverageStar: UIViewController {
+
+    @IBOutlet weak var phaseNumber: UITextField!
+    
+    @IBOutlet weak var starImage: UIImageView!
+    
+    @IBOutlet var descPhase: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func changeSlider(_ sender: UISlider) {
+        if(sender.value < 1){
+            phaseNumber.text = "Phase 1"
+            descPhase.text = "Huge clouds of dust and gases exist in space."
+            starImage.image = UIImage(named: "nebulacloud")
+        }
+        else if(sender.value < 2){
+            phaseNumber.text = "Phase 2"
+            descPhase.text = "The dust and gases are attracted to each other by their own gravity and getting denser and hotter until they fused together form a star."
+            starImage.image = UIImage(named: "lowmassstar")
+        }
+        else if(sender.value < 3){
+            phaseNumber.text = "Phase 3"
+            descPhase.text = "After the star has run out of hydrogen, it will lose its hydrostatic equilibrium and it is going to collapse again until the heliums themselves will be used as an energy source, which will then expand the star even more."
+            starImage.image = UIImage(named: "redgiant")
+        }
+        else if(sender.value < 4){
+            phaseNumber.text = "Phase 4"
+            descPhase.text = "The star expand so much that its outer atmosphere drifted out from the star in a process called supernova explosion."
+            starImage.image = UIImage(named: "nebulacloud")
+        }
+        else {
+            phaseNumber.text = "Phase 5"
+            descPhase.text = "The remnants of the average star is the white dwarf, which is the core of the star. It is incredibly dense and hot."
+            starImage.image = UIImage(named: "whitedwarf")
+        }
+    }
+
+}
